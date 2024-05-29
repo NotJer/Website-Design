1cf4019ae00e441e5105e612f132e4243a1882da
1chipp <166660222+1chipp@users.noreply.github.com>
2024-05-29 13:39:41 -0700

    Update display.js

--- display.js
+++ display.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function myFunction(elementId) {
+    var x = document.getElementById(elementId);
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
